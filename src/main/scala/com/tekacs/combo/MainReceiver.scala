d18ded1190765957a482b49af355ec04ef8035c9
Amar Sood (tekacs) <mail@tekacs.com>
2015-02-22 09:30:00 +0000

    Correct old file.

--- src/main/scala/com/tekacs/combo/MainReceiver.scala
+++ src/main/scala/com/tekacs/combo/MainReceiver.scala
@@ -4,7 +4,7 @@ import akka.actor.{Actor, ActorLogging}
 
 class MainReceiver extends Actor with ActorLogging {
   override def receive: Receive = {
-    case TopicActor.Fact(topic, data) =>
+    case Combo.Fact(topic, data) =>
       println(s"$topic: $data")
   }
 }
